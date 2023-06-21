d0644e6647bd11be8e259df819bcc1fa04fbd821
Gabriel <51428982+gabrieuz@users.noreply.github.com>
2023-06-20 19:57:38 -0500

    Aula 26/06

--- desafio01.js
+++ desafio01.js
@@ -1,7 +1,7 @@
 const fiveNumbers = [2, 3, 5, 7, 11];
-let [...{ n1, n2, n3, n4, n5 }] = fiveNumbers;
+let [n1, n2, n3, n4, n5] = fiveNumbers;
 
-let json = {
+let objeto = {
 	v1: n1,
 	v2: n2,
 	v3: n3,
@@ -9,6 +9,6 @@ let json = {
 	v5: n5,
 };
 
-json = JSON.stringify(json);
+json = JSON.stringify(objeto);
 json = JSON.parse(json);
-console.log(n1);
+console.log(json);
